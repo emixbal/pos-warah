800ca82fe51c7fa15634411a66fda3f15921eeec
iqbal <emixbal@gmail.com>
2022-08-18 17:29:22 +0700

    init anggota password

---
+++ src/resources/views/penjualan_detail/anggota_password.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="modal_anggota_password" tabindex="-1" role="dialog" aria-labelledby="modal-member">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Anggota Password Form</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="nominal_bayar_voucher_modal">Password Anggota</label>
+                    <input maxlength="8" type="password" class="form-control form-control-lg" autocomplete="off" readonly  onfocus="this.removeAttribute('readonly');" id="anggota_password_modal" >
+                </div>
+            </div><!-- .modal-body -->
+            <div class="modal-footer">
+                <button class="btn btn-info float-right" id="modal_password_ok" type="button">OK <i class="fa fa-arrow-right"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
